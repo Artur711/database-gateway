d771e1e0b3e2acfd60a942633ae7cfd5841cdc52
Artur Kubiak <arturkubiak9@gmail.com>
2022-01-06 17:48:27 +0100

    Change responses to string

--- src/main/java/com/java17/databasegateway/web/GatewayController.java
+++ src/main/java/com/java17/databasegateway/web/GatewayController.java
@@ -1,25 +1,22 @@
 package com.java17.databasegateway.web;
 
-import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/v1/test-db")
 public class GatewayController {
 
     @GetMapping("/get-response")
-    public ResponseEntity<String> getTesResponse() {
-        return ok("It's working");
+    public String getTesResponse() {
+        return "It's working";
     }
 
     @GetMapping("get-admin-response")
 //    @Secured("ROLE_ADMIN")
-    public ResponseEntity<String> getAdminResponse() {
-        return ok("You are administrator if you see this");
+    public String getAdminResponse() {
+        return "You are administrator if you see this";
     }
 }
